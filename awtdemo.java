fef0e4b49f1ecfaa98b9826fdf55c1e1283bf8c5
Asmita Jagdale <102750190+asmitajagdale@users.noreply.github.com>
2022-11-27 10:26:54 -0800

    Add files via upload

---
+++ awtdemo.java
@@ -0,0 +1,14 @@
+// AWT program
+
+import java.awt.*;
+public class awtdemo
+{
+ public static void main(String args[])
+ {
+  Frame a=new Frame();
+  a.setVisible(true);
+  a.setSize(400,500);
+  a.setTitle("AWT Frame");
+  a.setBackground(Color.blue);
+ }
+}
